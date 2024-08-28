116e263d41fa26afcd4d2049cf1e2e02474a90b6
Marcin Bogusz <bogusz.marcin@icloud.com>
2024-08-28 10:08:29 +0200

    Implement scoreboard, extend of the snake body.

--- scoreboard.py
+++ scoreboard.py
@@ -1,19 +1,27 @@
 from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ('Courier', 24, 'normal')
+
 
 class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
         self.points = 0
-        self.message = f"Scorecard: {self.points}"
         self.hideturtle()
         self.penup()
-        self.goto(-20, 280)
+        self.goto(0, 270)
         self.color("white")
-        self.write(self.message, align='center', font=('monaco', 10, 'normal'))
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Score: {self.points}", align=ALIGNMENT, font=FONT)
 
     def add_point(self):
         self.points += 1
-        self.message = f"Scorecard: {self.points}"
         self.clear()
-        self.write(self.message, align='center', font=('monaco', 10, 'normal'))
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
